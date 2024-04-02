e8927de940902d1bb8fae98f4bbc0d22739bfffc
NoorZehra23 <syeda.noor@dubizzlelabs.com>
2024-04-02 12:01:07 +0500

    Implantation of Day 6
    Used Events,
    Handled Lists,
    Props and Components

--- src/App.js
+++ src/App.js
@@ -1,59 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Search from './Components/Search';
 
-const TodoItem = ({ text }) =>
- <li>
-    {/* <span>{id} -</span> */}
-    <span>{text}</span>
-
- </li>;
-
-const TodoList = () => {
-  const [todos, setTodos] = useState([]);
-  const [newTodoText, setNewTodoText] = useState('');
-  const [nextId, setNextId] = useState(1);
-
-
-  const handleChange = (event) => {
-    setNewTodoText(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const newTodo = {
-      id: nextId,
-      text: newTodoText
-    };
-    setTodos((prevTodos) => [...prevTodos, newTodo]);
-    setNewTodoText('');
-    setNextId(nextId + 1);
-
-  };
-
-  return (
-    <div>
-      <h1>Todo List</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={newTodoText}
-          onChange={handleChange}
-          placeholder="Add new todo"
-        />
-        <button>Add</button>
-      </form>
-      <ul>
-        {todos.map((todo) => (
-          <TodoItem key={todo.id}   text={todo.text} />
-        ))}
-      </ul>
-    </div>
-  );
-};
 
 const App = () => {
+
   return (
     <div>
-      <TodoList />
+    <Search/>
     </div>
   );
 };
